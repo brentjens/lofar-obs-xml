c534f40abd6e7aebb86c91298fee49049d1434bf
Michiel Brentjens <brentjens@astron.nl>
2010-10-06 13:39:43 +0200

    Most of commandline handling in place.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='genvalobs',
+      version='0.1',
+      description='Generate XML file with LOFAR system validation observations',
+      author='Michiel Brentjens',
+      author_email='brentjens@astron.nl',
+      url='',
+      packages=[],
+      scripts=['genvalobs'],
+     )
