f37f33cb8558033c17d8cc9a62937694480cf2dd
Michiel Brentjens <brentjens@astron.nl>
2010-11-16 11:29:25 +0100

    Started v0.4 development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='genvalobs',
-      version='0.3',
+      version='0.4',
       description='Generate XML file with LOFAR system validation observations',
       author='Michiel Brentjens',
       author_email='brentjens@astron.nl',
