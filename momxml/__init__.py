1e7a8f9b054c59aba732211872e745f678f0e696
Michiel Brentjens <brentjens@astron.nl>
2012-11-05 13:33:34 +0100

    Made API more explicit in __init__.py. No more * imports there.

--- momxml/__init__.py
+++ momxml/__init__.py
@@ -1,7 +1,10 @@
-from momxml.angles import *
-from momxml.utilities import *
-from momxml.momformats import *
-from momxml.targetsource import *
-from momxml.observation import *
-from momxml.sourcecatalogue import *
+from momxml.angles    import signum, sign_char, int_from_sign_char, Angle
+
+from momxml.utilities import unique, flatten_list, lofar_sidereal_time
+from momxml.utilities import station_list, validate_enumeration
+
+from momxml.targetsource    import SourceSpecificationError, TargetSource
+from momxml.sourcecatalogue import SourceCatalogue
+from momxml.observation     import Beam, Observation, as_xml_mom_project
+
 import ephem
