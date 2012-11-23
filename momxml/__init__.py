ce5d59d2887f33f83ab33311d255f48b07792c1f
Michiel Brentjens <brentjens@astron.nl>
2012-11-23 10:32:43 +0100

    Added InvalidStationSetError to interface

--- momxml/__init__.py
+++ momxml/__init__.py
@@ -2,6 +2,7 @@ from momxml.angles    import signum, sign_char, int_from_sign_char, Angle
 
 from momxml.utilities import unique, flatten_list, lofar_sidereal_time
 from momxml.utilities import station_list, validate_enumeration, next_date_with_lofar_lst
+from momxml.utilities import InvalidStationSetError
 
 from momxml.targetsource    import SourceSpecificationError, NoSimbadCoordinatesError
 from momxml.targetsource    import TargetSource, simbad
