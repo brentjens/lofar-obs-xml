d8f5b47401b6d7f9f6a21968c8ee395dcb0a8bbf
Michiel Brentjens <brentjens@astron.nl>
2013-01-30 14:10:45 +0100

    Simplified cep-install.sh

--- cep-install.sh
+++ cep-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-comm="rsync -avz --delete brentjens@dop95.astron.nl:`basename $1` `basename $1`"
-echo $comm
-rsync -avz $1 brentjens@dop95.astron.nl: && lfc lfe001 "$comm"
+USER=$1
+
+rsync -avz --delete -e "ssh -A ${USER}@portal.lofar.eu ssh" ./ ${USER}@lhn001:genvalobs/
